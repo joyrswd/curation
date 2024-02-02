113eefaa15f8c45ad6eef169852f25273872e92b
joyr <swd@joyr.net>
2024-02-02 18:17:08 +0900

    一覧の間隔を微調整

--- app/_/components/entry.tsx
+++ app/_/components/entry.tsx
@@ -12,19 +12,19 @@ const Entry = async ({ id }: { id: string }) => {
     const time = new Date(record.date).toLocaleString('ja-JP', { hour: '2-digit', minute: '2-digit' });
     const date = new Date(record.date).toLocaleString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '-');
     return (
-        <div className="w-full lg:max-w-sm">
-            <div className="h-full flex flex-col p-4 md:pb-9 bg-gradient-to-t mx-1 from-white via-neutral-200/75 via-10% to-white to-50%">
+        <div className="w-full lg:max-w-sm mx-1 mb-2">
+            <div className="h-full flex flex-col p-4 md:pb-9 bg-gradient-to-t from-white via-neutral-200/75 via-10% to-white to-50%">
                 <div className="flex">
                     <p className="w-12 text-center self-center mr-3 leading-none">
                         {record.image && <Image alt="blog" src={record.image} width={100} height={100} className="w-7 h-7 m-auto md:w-10 md:h-10 rounded-full flex-shrink-0 object-cover object-center" />}
                         <span className="text-gray-500 text-xs">{time}</span>
                     </p>
-                    <a href={record.link} target="feed" className='w-full'><h1 className="title-font self-center font-medium text-gray-700 leading-6 md:text-xl">{record.title}</h1></a>
+                    <Link href={record.link} target="feed" className='w-full flex'><h1 className="self-center w-full pb-2.5 title-font font-medium text-gray-700 text-sm md:text-lg">{record.title}</h1></Link>
                 </div>
-                <div className="my-2 mx-3 grow hidden md:block">
+                <div className="m-2 pb-4 grow hidden md:block">
                     <p className="leading-relaxed text-sm"><a href={record.link} target="feed">{record.intro.substring(0, 200)}</a></p>
                 </div>
-                <div className="self-end w-full flex md:items-end text-xs mt-2">
+                <div className="self-end w-full flex md:items-end text-xs mt-1">
                     <div className='w-12 mr-3 text-center'>
                         <Link href={{ pathname: '/find', query: { date: date } }} className='flex md:flex-col'>
                             <span className="text-gray-500 md:pb-2 md:mb-2 md:border-b-2 border-gray-400/50">{month}</span>
