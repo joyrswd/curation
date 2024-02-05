80cad54f54ed9b8e96a4354cca3ae46538a3c01a
joyr <swd@joyr.net>
2024-02-05 19:50:57 +0900

    フィード更新の要否チェック追加

--- app/_/batch/crawler.ts
+++ app/_/batch/crawler.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {upsert} from '../lib/MeiliSearch';
+import {upsert, isNew} from '../lib/MeiliSearch';
 import {loadSiteFeed} from '../lib/RssLoader';
 const args = process.argv.slice(2);
 const minutes:number = (args[0])? parseInt(args[0])??1 : 1; //引数で実行間隔（分）を指定
@@ -7,9 +7,7 @@ const triggerTime = minutes * 60 * 1000;
 const configPath = '../conf/rss.ts';
 
 const main = async (isInstant:boolean) => {
-    await loadSiteFeed(configPath, isInstant, async (item:any, siteName:string, siteHome:string) => {
-        await upsert(item, siteName, siteHome);
-    }).catch(err => console.error('An error occurred:', err))
+    await loadSiteFeed(configPath, isInstant, isNew, upsert).catch(err => console.error('An error occurred:', err))
     .finally(() => (isInstant) ? process.exit(0) : setTimeout(main, triggerTime, isInstant));
 };
 
